da005027b5386b03819bd98f60feaf9814408855
james carroll <jacarrol@g.cofc.edu>
2015-08-29 06:15:43 -0400

    DEFEATED THE EVIL JSON.

--- js/superPowers.js
+++ js/superPowers.js
@@ -1,15 +1,7 @@
 var categories = [["Almighty Powers","Almighty_Powers"],["Mental Power","Mental_Power"],["Physical Powers","Personal_Physical_Powers"],["Mimicry","Mimicry"],["Enhancements", "Enhancements"],["Manipulations","Manipulations"]];
 
-function Get(selection){
-    var Httpreq = new XMLHttpRequest();
-    var url = "http://powerlisting.wikia.com/api/v1/Articles/List?expand=1&category=";
-    var urlCall = url.concat(categories[selection][1]);
-    Httpreq.open("GET",urlCall,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;
-}
-
-function getPowers(selection){
-    var json = JSON.parse(Get(selection));
-    return json.toString();
+function getPowers(string){
+    var json;
+    json = JSON.parse(string);
+    return json;
 }
